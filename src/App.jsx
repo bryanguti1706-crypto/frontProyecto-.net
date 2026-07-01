90868208c379addb3d0b66eb7e819f6ba8c54540
Runor1 <alepcadio12@gmail.com>
2026-06-30 21:18:14 -0600

    Compilación correcta

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,20 @@ import Habitaciones from './pages/Admin/Habitaciones/Habitaciones.jsx'
 import Mascotas from './pages/Admin/Mascotas/Mascotas.jsx'
 import Citas from './pages/Admin/Citas/Citas.jsx'
 
+
+import Navbar from "./components/Navbar/AppNavbar.jsx";
+import Login from "./pages/Login/Login.jsx";
+import Register from "./pages/Registro/Registro.jsx";
+
 function App() {
   return (
     <BrowserRouter>
+    <Navbar />
       <Routes>
-        <Route path="/" element={<Inicio />} />
+        <Route path="/" element={<Login />} />
 
+
+        <Route path="/Registro" element={<Register />} />
         <Route path="/admin/users" element={<Usuarios />} />
         <Route path="/admin/tipomascotas" element={<TipoMascotas />} />
         <Route path="/admin/habitaciones" element={<Habitaciones />} />
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './assets/css/style.css'
 import App from './App.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
